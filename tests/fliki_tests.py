3020e0e9f12f2b705cdcf0a5ae7903aa95e8958c
blueblank <blueblank@gmail.com>
2014-03-15 20:11:25 -0400

    revised malformed test data

--- tests/fliki_tests.py
+++ tests/fliki_tests.py
@@ -15,19 +15,17 @@ class FlikiBase(FlikiTest):
         r = self._get('/test/test_page/')
         self.assertIn("Edit Page Content", r.data)
 
-    # doesn't work in test -- 400 bad request
-    #def test_edit_page(self):
-    #    r = self._post('/test/save', data=dict(url='test_page', edit_content='a test_page'))
-    #    self.assertIn("a test_page", r.data)
+    def test_edit_page(self):
+        r = self._post('/test/save', data=dict(pagekey='test_page', edit_content='a test_page'))
+        self.assertIn("a test_page", r.data)
 
     def test_display_page(self):
         r = self._get('/test/random_page/')
         self.assertIn("A random page", r.data)
 
-    # doesn't work in test -- 400 bad request
-    #def test_move_page(self):
-    #    r = self._post('/test/move', data=dict(old='random_page', new='a/new/random/page'))
-    #    self.assertIn("A random page", r.data)
+    def test_move_page(self):
+        r = self._post('/test/move', data=dict(oldkey='random_page', newkey='a/new/random/page'))
+        self.assertIn("A random page", r.data)
 
     def test_delete_page(self):
         r = self._post('/test/delete', data=dict(delete='random_page'))
