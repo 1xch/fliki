ddbbcfac946e20f8d4d9749c3af0eccdc01c3a50
blueblank <blueblank@gmail.com>
2014-03-19 18:56:00 -0400

    functionally ok in python 2.7, 3.3 & 3.4'

--- tests/fliki_tests.py
+++ tests/fliki_tests.py
@@ -8,30 +8,30 @@ class FlikiBase(FlikiTest):
 
     def test_has_index(self):
         r = self._get('/test/')
-        self.assertNotIn("Title: index\nSummary: base page for wiki", r.data)
-        self.assertIn("Welcome to your flask-wiki", r.data)
+        self.assertNotIn(b"Title: index\nSummary: base page for wiki", r.data)
+        self.assertIn(b"Welcome to your flask-wiki", r.data)
 
     def test_new_page(self):
         r = self._get('/test/test_page/')
-        self.assertIn("Edit Page Content", r.data)
+        self.assertIn(b"Edit Page Content", r.data)
 
     def test_edit_page(self):
         r = self._post('/test/save', data=dict(pagekey='test_page', edit_content='a test_page'))
-        self.assertIn("a test_page", r.data)
+        self.assertIn(b"a test_page", r.data)
 
     def test_display_page(self):
         r = self._get('/test/random_page/')
-        self.assertIn("A random page", r.data)
+        self.assertIn(b"A random page", r.data)
 
     def test_move_page(self):
         r1 = self._post('/test/move', data=dict(oldkey='random_page', newkey='new/random_page'))
         r2 = self._get('/test/new/random_page')
         r3 = self._get('/test/random_page')
-        self.assertIn("A random page", r1.data)
+        self.assertIn(b"A random page", r1.data)
         self.assertEqual(r1.data, r2.data)
-        self.assertNotIn("A random page", r3.data)
+        self.assertNotIn(b"A random page", r3.data)
 
 
     def test_delete_page(self):
         r = self._post('/test/delete', data=dict(delete='random_page'))
-        self.assertIn("index", r.data)
+        self.assertIn(b"index", r.data)
